93aa497444ee1532516dd7a723f6f4813813a265
Andrii Dolhopolov <dolgopolov_a@ukr.net>
2024-12-08 11:30:13 +0200

    save_08_12_2024

--- words/w_17.js
+++
@@ -1,3 +0,0 @@
-﻿DicArray.push({eng: "school ", trk: "|skuːl|", ukr: "школа", snd: "school .mp3"});
-DicArray.push({eng: "bag", trk: "|bæɡ|", ukr: "сумка", snd: "bag.mp3"});
-DicArray.push({eng: "pencil ", trk: "|ˈpensl|", ukr: "олівець", snd: "pencil .mp3"});
